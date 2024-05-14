53d07544cad4e2d5c6897bf59ddcf7573e7afdc1
urban <urban@example.com>
2024-05-14 22:14:30 +0200

    update the script

--- run_bitcoin_node.sh
+++ run_bitcoin_node.sh
@@ -4,7 +4,7 @@ source ./Scripts/utils.sh
 
 if [ $SESSION_BITCOIN_COUNT -eq 0 ]; then
     p "Starting up stratum bitcoin node..."
-    screen -d -m -c ../Configurations/screen.screenrc -S "stratum_bitcoin_node" ../Bin/bitcoin-sv2-tp-0.1.2/bin/bitcoind -sv2 -sv2port=8442 -prune=1092
+    screen -d -m -c Configurations/screen.screenrc -S "stratum_bitcoin_node" Bin/bitcoin-sv2-tp-0.1.2/bin/bitcoind -sv2 -sv2port=8442 -prune=1092
     #./status.sh
 elif [ $SESSION_BITCOIN_COUNT -eq 1 ]; then
     p "bitcoin node is already running"
