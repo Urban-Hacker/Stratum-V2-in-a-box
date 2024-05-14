acf290821f378d1936eb737a6596cd019c75ebea
urban <urban@example.com>
2024-05-15 00:22:32 +0200

    update

--- run_proxy.sh
+++ run_proxy.sh
@@ -2,4 +2,9 @@
 source ./Scripts/global.sh
 source ./Scripts/utils.sh
 
-p To run the Stratum V2 proxy make sure you are running
+if [ $SESSION_BITCOIN_COUNT -eq 0 ]; then
+    p_err "Bitcoin deamon must run in order to start the proxy"
+    p_fatal "run ./run_bitcoin_node.sh start to launch it"
+fi
+
+./Bin/demand_all_in_one_sv2-x86_64-linux-gnu -a $1
